87aa260530a185832d6ca37e0c4298b0d7862ba7
Kwak Seong Joon <seongjoonkwak@inu.ac.kr>
2025-05-27 16:43:57 +0900

    버그 픽스

--- frontend/src/store/useUserStore.js
+++ frontend/src/store/useUserStore.js
@@ -3,6 +3,9 @@ import { create } from "zustand";
 export const useUserStore = create((set) => ({
   user: null,
   setUser: (user) => set({ user }),
-  logout: () => set({ user: null }),
+  logout: () => {
+    localStorage.removeItem("currentUser");
+    set({ user: null });
+  },
   // 필요하다면 점수, 게임 상태 등도 추가
 }));
